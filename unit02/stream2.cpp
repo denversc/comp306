4720d97e28723bfcc5867c0774de182772f936a5
Denver Coneybeare <denver.coneybeare@gmail.com>
2014-01-13 20:21:01 -0500

    stream2.cpp added

---
+++ unit02/stream2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is fifteen in decimal: " << dec << 15 << endl;
+    cout << "This is fifteen in hexadecimal: " << hex << 15 << endl;
+    cout << "This is fifteen in octal: " << oct << 15 << endl;
+    cout << "This is a float: " << 3.14159 << endl;
+    cout << "The escape character is: " << char(27) << endl;
+    return 0;
+}
